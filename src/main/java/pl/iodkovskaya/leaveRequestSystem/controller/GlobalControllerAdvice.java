a4fed89565f6e852741aee0158476cdeceae1173
Alena Iadkouskaya <koptewka@mail.ru>
2024-11-13 23:18:04 +0100

    1. Praca nad testy do VacationBalanceController

--- src/main/java/pl/iodkovskaya/leaveRequestSystem/controller/GlobalControllerAdvice.java
+++ src/main/java/pl/iodkovskaya/leaveRequestSystem/controller/GlobalControllerAdvice.java
@@ -7,7 +7,6 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
-import org.springframework.web.bind.annotation.ResponseStatus;
 import pl.iodkovskaya.leaveRequestSystem.exception.InvalidOperationException;
 import pl.iodkovskaya.leaveRequestSystem.exception.RoleExistException;
 import pl.iodkovskaya.leaveRequestSystem.exception.StatusException;
--- src/test/java/pl/iodkovskaya/leaveRequestSystem/controller/VacationBalanceControllerTests.java
+++ src/test/java/pl/iodkovskaya/leaveRequestSystem/controller/VacationBalanceControllerTests.java
@@ -1,18 +1,15 @@
 package pl.iodkovskaya.leaveRequestSystem.controller;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
-import jakarta.persistence.EntityNotFoundException;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
-import org.mockito.Mock;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.http.MediaType;
 import org.springframework.security.test.context.support.WithMockUser;
 import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.web.servlet.MockMvc;
-import org.springframework.transaction.annotation.Transactional;
 import pl.iodkovskaya.leaveRequestSystem.model.dto.VacationBalanceDto;
 import pl.iodkovskaya.leaveRequestSystem.model.entity.role.RoleEntity;
 import pl.iodkovskaya.leaveRequestSystem.model.entity.user.UserEntity;
@@ -20,7 +17,6 @@ import pl.iodkovskaya.leaveRequestSystem.reposityry.RoleRepository;
 import pl.iodkovskaya.leaveRequestSystem.reposityry.UserRepository;
 import pl.iodkovskaya.leaveRequestSystem.service.VacationBalanceService;
 
-import static org.mockito.Mockito.verify;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
@@ -28,6 +24,7 @@ import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.
 @SpringBootTest
 @ActiveProfiles("test")
 @AutoConfigureMockMvc
+@WithMockUser(username = "manager@gmail.com", password = "1", roles = "USER")
 public class VacationBalanceControllerTests {
     @Autowired
     private MockMvc mockMvc;
@@ -35,38 +32,53 @@ public class VacationBalanceControllerTests {
     @Autowired
     private ObjectMapper objectMapper;
 
-    @MockBean
+    @Autowired
     private UserRepository userRepository;
 
-    @MockBean
+    @Autowired
     private RoleRepository roleRepository;
 
-    @MockBean
+    @Autowired
     private VacationBalanceService vacationBalanceService;
 
+    @BeforeEach
+    void setUp() {
+        userRepository.deleteAll();
+        roleRepository.deleteAll();
+
+    }
+
     @Test
-    @Transactional
-    @WithMockUser(username = "manager@gmail.com", password = "1", roles = "USER")
     void should_create_vacation_balance_when_valid_dto() throws Exception {
         // given
-        RoleEntity roleEntity = roleRepository.findByRoleName("ROLE_USER")
-                .orElseGet(() -> roleRepository.save(new RoleEntity("ROLE_USER")));
-
-        UserEntity userEntity = new UserEntity("manager@gmail.com", "1", "LastName", "FirstName", "manager@gmail.com",
-                roleEntity, true);
-
+        RoleEntity roleUser = new RoleEntity("ROLE_USER");
+        UserEntity userEntity = new UserEntity("user@gmail.com", "1", "LastName", "FirstName", "user@gmail.com",
+                roleUser, true);
         userRepository.save(userEntity);
-
         VacationBalanceDto vacationBalanceDto = new VacationBalanceDto(userEntity.getUserId(), 20, 5);
 
-        // when
+        // when & then
         mockMvc.perform(post("/api/vacation-balance/new")
                         .contentType(MediaType.APPLICATION_JSON)
                         .content(objectMapper.writeValueAsString(vacationBalanceDto)))
                 .andExpect(status().isCreated())
                 .andExpect(content().string("The data regarding the remaining days has been successfully added"));
 
-        // then
-        verify(vacationBalanceService).addRecord(vacationBalanceDto);
+    }
+
+    @Test
+    void shouldReturnBadRequestWhenInvalidDto() throws Exception {
+        // given
+        RoleEntity roleUser = new RoleEntity("ROLE_USER");
+        UserEntity userEntity = new UserEntity("user@gmail.com", "1", "LastName", "FirstName", "user@gmail.com",
+                roleUser, true);
+        userRepository.save(userEntity);
+        VacationBalanceDto invalidDto = new VacationBalanceDto(null, 20, 5);
+
+        // when & then
+        mockMvc.perform(post("/api/vacation-balance/new")
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .content(objectMapper.writeValueAsString(invalidDto)))
+                .andExpect(status().isBadRequest());
     }
 }
