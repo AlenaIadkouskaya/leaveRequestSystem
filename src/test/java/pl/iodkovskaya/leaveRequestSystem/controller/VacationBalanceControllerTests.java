7513ec675954cd8ce529e0e74fa8e2201138e552
Alena Iadkouskaya <koptewka@mail.ru>
2024-11-13 20:55:34 +0100

    1. Praca nad testem should_create_vacation_balance_when_valid_dto()

--- src/test/java/pl/iodkovskaya/leaveRequestSystem/controller/VacationBalanceControllerTests.java
+++ src/test/java/pl/iodkovskaya/leaveRequestSystem/controller/VacationBalanceControllerTests.java
@@ -2,25 +2,22 @@ package pl.iodkovskaya.leaveRequestSystem.controller;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
 import org.junit.jupiter.api.Test;
-import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.http.MediaType;
-import org.springframework.security.core.Authentication;
-import org.springframework.security.core.context.SecurityContext;
-import org.springframework.security.core.context.SecurityContextHolder;
-import org.springframework.security.test.web.servlet.request.SecurityMockMvcRequestPostProcessors;
+import org.springframework.security.test.context.support.WithMockUser;
 import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.transaction.annotation.Transactional;
 import pl.iodkovskaya.leaveRequestSystem.model.dto.VacationBalanceDto;
 import pl.iodkovskaya.leaveRequestSystem.model.entity.role.RoleEntity;
 import pl.iodkovskaya.leaveRequestSystem.model.entity.user.UserEntity;
+import pl.iodkovskaya.leaveRequestSystem.reposityry.RoleRepository;
+import pl.iodkovskaya.leaveRequestSystem.reposityry.UserRepository;
 import pl.iodkovskaya.leaveRequestSystem.service.VacationBalanceService;
 
 import static org.mockito.Mockito.verify;
-import static org.mockito.Mockito.when;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
@@ -34,36 +31,30 @@ public class VacationBalanceControllerTests {
     private ObjectMapper objectMapper;
     @Mock
     private VacationBalanceService vacationBalanceService;
-    @InjectMocks
-    private VacationBalanceController vacationBalanceController;
-    @MockBean
-    private SecurityContext securityContext;
-    @MockBean
-    private Authentication authentication;
+    @Autowired
+    private UserRepository userRepository;
+    @Autowired
+    private RoleRepository roleRepository;
 
     @Test
-    void createVacationBalance_ShouldReturnCreated_WhenValidDto() throws Exception {
+    @Transactional
+    @WithMockUser(username = "manager@gmail.com", password = "1", roles = "USER")
+    void should_create_vacation_balance_when_valid_dto() throws Exception {
         // given
-        RoleEntity roleEntity = new RoleEntity("ROLE_USER");
-        VacationBalanceDto vacationBalanceDto = new VacationBalanceDto(1L, 20, 5);
-        UserEntity userEntity = new UserEntity("manager@gmail.com", "1", "LastName", "FirstName", "user1@mail.com",
+        RoleEntity roleEntity = roleRepository.findByRoleName("ROLE_USER").orElseThrow();
+        UserEntity userEntity = new UserEntity("manager@gmail.com", "1", "LastName", "FirstName", "manager@gmail.com",
                 roleEntity, true);
-
-        when(authentication.getPrincipal()).thenReturn(userEntity);
-        when(authentication.getName()).thenReturn(userEntity.getEmail());
-        when(securityContext.getAuthentication()).thenReturn(authentication);
-        SecurityContextHolder.setContext(securityContext);
+        userRepository.save(userEntity);
+        VacationBalanceDto vacationBalanceDto = new VacationBalanceDto(userEntity.getUserId(), 20, 5);
 
         // when
         mockMvc.perform(post("/api/vacation-balance/new")
                         .contentType(MediaType.APPLICATION_JSON)
-                        .content(objectMapper.writeValueAsString(vacationBalanceDto))
-                        .with(SecurityMockMvcRequestPostProcessors.httpBasic("manager@gmail.com", "1")))
+                        .content(objectMapper.writeValueAsString(vacationBalanceDto)))
                 .andExpect(status().isCreated())
                 .andExpect(content().string("The data regarding the remaining days has been successfully added"));
 
-
         // then
-        verify(vacationBalanceService).addRecord(vacationBalanceDto);
+        //verify(vacationBalanceService).addRecord(vacationBalanceDto);
     }
 }
